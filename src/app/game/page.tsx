99ed202fa83faac9a7cae5c721b75f34dd6a9cfe
Marcos <marcosraach.1@gmail.com>
2024-09-05 08:23:04 -0300

    feat: update GameBoard component with optimized image shuffling and initialization

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,5 +1,5 @@
 import { Flex, Text } from '@chakra-ui/react'
-import { GameBoard } from '../components/game-board'
+import { GameBoard } from '@/components/game-board'
 
 export default function Game() {
   return (
